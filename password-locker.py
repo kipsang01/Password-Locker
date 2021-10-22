9c2611cd587d97233cc33ec4a8f4dddca3ea619b
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-22 20:36:42 +0300

    Add function to show user optins

---
+++ password-locker.py
@@ -0,0 +1,15 @@
+def main():
+    
+    #function to display choices
+    def show_options():
+        print('Choose what you want to do:\n\
+                1. Create new Credential \n\
+                2. Display saved Credentials\n\
+                3. Delete a Credential\n\
+                4. Log Out\n\
+                5. Exit')
+        
+    show_options()  
+if __name__ == '__main__':
+    
+    main()
