00f168bfe7fb6cdf9a1426777e5b8b292671f55f
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-22 20:38:20 +0300

    Add login function

--- password-locker.py
+++ password-locker.py
@@ -1,5 +1,7 @@
 def main():
     
+    users = []
+    
     #function to display choices
     def show_options():
         print('Choose what you want to do:\n\
@@ -9,7 +11,24 @@ def main():
                 4. Log Out\n\
                 5. Exit')
         
-    show_options()  
+        
+    #function login
+    def login():
+        print('please login now:')
+        verify_username = input('Username: ')
+        verify_password = input('Password: ')
+        
+        if verify_username not in users:
+            print('User does not exist.Create account first')
+            main()
+            
+        #verify login details
+        while verify_username != new_user.username or verify_password != new_user.password:
+            print("invalid credentials! Try again")
+            verify_username = input('Username: ')
+            verify_password = input('Password: ')   
+        
+    # show_options()  
 if __name__ == '__main__':
     
     main()
