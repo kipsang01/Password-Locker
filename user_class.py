88a4227681bbe8a675a20ebbd0620f8635d43b54
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-22 20:19:26 +0300

    Add method to retrieve credentials

--- user_class.py
+++ user_class.py
@@ -10,3 +10,10 @@ class User:
         new_credential = Credential(acc_name, acc_username, acc_password )
         self.credentials.append(new_credential)
         print("New Credential saved successfully")
+        
+        
+    def see_credentials(self):
+        i = 0
+        for credential in self.credentials:
+            print( str(i) +'  Account: '+credential.acc_name +' Username: '+ credential.acc_username +' Password: '+ credential.acc_password)
+            i +=1
