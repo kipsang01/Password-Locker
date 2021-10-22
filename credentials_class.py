6100f1f86119f2f582d0cedaa856c9e093904346
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-22 20:22:30 +0300

    Initialize credential class

--- credentials_class.py
+++ credentials_class.py
@@ -1,2 +1,5 @@
 class Credential:
-    pass
+    def __init__(self,acc_name, acc_username, acc_password):
+        self.acc_name = acc_name
+        self.acc_username = acc_username
+        self.acc_password = acc_password
