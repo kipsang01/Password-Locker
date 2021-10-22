e68e4077f4f0b647518b1891feaa6d3c832c910f
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-22 20:16:13 +0300

    Add user method to create credential

--- user_class.py
+++ user_class.py
@@ -1,6 +1,11 @@
 class User:
-     credentials = []
+    credentials = []
      
-     def __init__(self,username, password):
+    def __init__(self,username, password):
         self.username  = username
         self.password = password
+        
+    def create_new_credential(self, acc_name, acc_username, acc_password):
+        new_credential = Credential(acc_name, acc_username, acc_password )
+        self.credentials.append(new_credential)
+        print("New Credential saved successfully")
